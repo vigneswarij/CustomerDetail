3bd133d763951c65db792db5990747839362297f
vigneswarij <svigneswari.mca@gmail.com>
2024-01-27 14:36:02 +0530

    first commit

---
+++ src/main/java/com/sample/consumer/CustomerDetailApplication.java
@@ -0,0 +1,13 @@
+package com.sample.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDetailApplication.class, args);
+	}
+
+}
